23be0a0bc7267cfa658baa3e7664911c51ecf491
Kaioecker <kaio.ecker@estudante.ifms.edu.br>
2025-01-28 21:08:47 -0300

    Prova dia 28/01

--- FE2_DP/js/script.js
+++ FE2_DP/js/script.js
@@ -1,3 +1,7 @@
-    function menu(pagina){
-        $( "#conteudo" ).load( "./pages/"+pagina+".html" );
+    function menu(pagina) {
+        $("#conteudo").slideUp(2000, () => {
+            $("#conteudo").load("./pages/" + pagina + ".html", () => {
+                $("#conteudo").slideDown(1500);
+            });
+        });
     }
